4236d7c2290bf7a7fe3e6e5c71da2177a3a76ebd
FalledKen <silicikenzo@gmail.com>
2026-03-23 15:07:22 +0100

    Ajout de controllers

---
+++ src/Controller/ControllerEspaceEleve.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+class PageInvite
+{
+    private \Twig\Environment $twig;
+
+    public function __construct(\Twig\Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    /**
+     * Récupère les données et affiche la page invité.
+     */
+    public function render(): void
+    {
+        echo $this->twig->render('espaceEleve.html.twig', [
+            'page'   => 'espace_eleve',
+            'title'  => 'Espace élève',
+            'platform_name' => 'CESI-STAGES',
+        ]);
+    }
+}
