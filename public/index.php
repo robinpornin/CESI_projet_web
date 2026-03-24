97040263b7f8df27d1fa7af33ac3cb560de7c71e
FalledKen <silicikenzo@gmail.com>
2026-03-24 09:31:30 +0100

    Correction pour affichage erreur

--- public/index.php
+++ public/index.php
@@ -1,10 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
-declare(strict_types=1);
 
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../src/Controller/ControllerAccueil.php';
