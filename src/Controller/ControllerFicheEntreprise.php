11ae722ed6448482f3a117bece0c1c3cb4f8d421
FalledKen <silicikenzo@gmail.com>
2026-03-24 10:21:24 +0100

    Correction de l'emplacement des controller

---
+++ src/Controller/ControllerFicheEntreprise.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+class PageAccueil
+{
+    private \Twig\Environment $twig;
+    
+
+    public function __construct(\Twig\Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    /**
+     * Récupère les données nécessaires et affiche la page.
+     */
+    public function render(): void
+    {
+        // Exemple : récupérer des articles depuis la BDD
+
+        // Passe les données à Twig et affiche
+        echo $this->twig->render('ficheEntreprise.html.twig', [
+            'page'     => 'fiche_entreprise',
+            'title'    => 'Fiche entreprise',
+            'platform_name' => 'CESI-STAGES',
+        ]);
+    }
+}
